9a9a9a5aa397622a49606339d78d59a4e5198b0d
admauc <admauc@yandex.ru>
2017-01-19 06:34:23 +0300

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Auctions.WebUI.Startup))]
+namespace Auctions.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
