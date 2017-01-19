341d32363ac878c5b94d55373cd08a104c469290
admauc <admauc@yandex.ru>
2017-01-19 06:29:33 +0300

    Add files via upload

---
+++ Аукционы.Web-интерфейс/Controllers/LotController.cs
@@ -0,0 +1,106 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using Auctions.Domain.Entities;
+using Auctions.WebUI.Models;
+using Auctions.Domain.Concrete;
+using System.Data.Entity;
+
+namespace Auctions.WebUI.Controllers
+{
+    
+    public class LotController : Controller
+    {
+        EFDbContext db = new EFDbContext();
+        public int PageSize = 12;
+        public LotController()
+        {
+        }
+
+        public ViewResult List(string category, int page = 1)
+        {
+
+        LotsListViewModel model = new LotsListViewModel
+        {
+            Lots = db.Lots
+            .Where(p => category == null || p.Category == category)
+            .OrderBy(p => p.LotID)
+            .Skip((page - 1) * PageSize)
+            .Take(PageSize),
+            PagingInfo = new PagingInfo
+            {
+                CurrentPage = page,
+                ItemsPerPage = PageSize,
+                TotalItems = category == null ?
+                db.Lots.Count() :
+                db.Lots.Where(e => e.Category == category).Count()
+            },
+            CurrentCategory = category
+
+        };
+        return View(model);
+        }
+
+        public FileContentResult GetImage(int LotID)
+        {
+            Lot lot = db.Lots.FirstOrDefault(p => p.LotID == LotID);
+            if (lot != null)
+            {
+                return File(lot.ImageData, lot.Preview);
+            }
+            else if(lot == null)
+            {
+                Response.Redirect("/Lot/NotFound"); 
+            }
+            return null;
+        }
+
+        public ActionResult SearchForName(string searchString)
+        {
+            IQueryable<Lot> lots = db.Lots.Where(p => p.Name.Contains(searchString));
+            return View(lots);
+        }
+
+        [Authorize]
+        [HttpGet]
+        public ViewResult LinkLot(int LotID)
+        {
+            Lot lot = db.Lots.FirstOrDefault(p => p.LotID.Equals(LotID));
+            return View(lot);
+        }
+
+        [Authorize]
+        [HttpGet]
+        public ActionResult MyLot(int LotID)
+        {
+            IQueryable<Lot> lot = db.Lots.Where(p => p.LotID.Equals(LotID));
+            return View(lot);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult ShiftLot(int LotID, decimal Bid)
+        {
+            Lot lot = db.Lots.FirstOrDefault(p => p.LotID == LotID);
+            if (lot != null && Bid > 0)
+            {
+                decimal a = lot.CurrentPrice;
+                lot.CurrentPrice = (a + Bid);
+                lot.UserEmailBid = User.Identity.Name;
+                db.SaveChanges();
+            }
+            return RedirectToAction("LinkLot", lot);
+        }
+
+        [Authorize]
+        public ActionResult LotBuy(int LotID)
+        {
+            Lot lot = db.Lots.FirstOrDefault(p => p.LotID == LotID);
+            return View(lot);
+        }
+
+        public ViewResult NotFound()
+        {
+            return View();
+        }
+    }
+}
---
+++ Аукционы.Web-интерфейс/Controllers/UserController.cs
@@ -0,0 +1,116 @@
+﻿using Auctions.Domain.Abstract;
+using Auctions.Domain.Entities;
+using System;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Auctions.WebUI.Controllers
+{
+    public class UserController : Controller
+    {
+        private ILotRepository repository;
+        public UserController(ILotRepository repo)
+        {
+            repository = repo;
+        }
+
+        [Authorize]
+        public ViewResult Index()
+        {
+            IQueryable<Lot> lots = repository.Lots.Where(p => p.UserEmail.Contains(User.Identity.Name));
+            return View(lots);
+        }
+
+        [Authorize]
+        [HttpGet]
+        public ViewResult Edit(int LotId)
+        {
+        Lot lot = repository.GetLotById(LotId);
+        return View(lot);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult Edit(Lot lot, HttpPostedFileBase image, int days)
+        {
+            if (ModelState.IsValid)
+            {
+                if (image == null)
+                {
+                    return View("Edit");
+                }
+                if (image != null)
+                {
+                    lot.StartDate = DateTime.Now;
+                    lot.EndDate = (lot.StartDate + new TimeSpan(days, 0, 0, 0));
+                    lot.UserEmail = User.Identity.Name;
+                    lot.UserEmailBid = User.Identity.Name;
+                    lot.Preview = image.ContentType;
+                    lot.ImageData = new byte[image.ContentLength];
+                    image.InputStream.Read(lot.ImageData, 0, image.ContentLength);
+                }
+                repository.SaveLot(lot);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                // there is something wrong with the data values
+                return View(lot);
+            }  
+        }
+
+        [Authorize]
+        public ViewResult Create()
+        {
+
+            return View("Edit", new Lot());
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult Delete(int LotID)
+        {
+            Lot deletedLot = repository.DeleteLot(LotID);
+            if (deletedLot != null)
+            {
+                TempData["message"] = string.Format("{0} Удалён", deletedLot.Name);
+            }
+            return RedirectToAction("Index");
+        }
+
+        [Authorize]
+        [HttpGet]
+        public ActionResult CreateBid()
+        {
+            IQueryable<Lot> lots = repository.Lots.Where(p => p.UserEmailBid.Contains(User.Identity.Name));
+            return View(lots);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult ShiftBid(int LotID, decimal Bid)
+        {
+            IQueryable<Lot> lots = repository.Lots.Where(p => p.UserEmailBid.Contains(User.Identity.Name));
+            if (Bid > 0)
+            {
+                Lot lot = repository.GetLotById(LotID);
+                if (lot != null)
+                {
+                    decimal a = lot.CurrentPrice;
+                    lot.CurrentPrice = (a + Bid);
+                    lot.UserEmailBid = User.Identity.Name;
+                    repository.SaveLot(lot);
+                }
+                return RedirectToAction("CreateBid");
+            }
+            return View(lots);
+        }
+
+        public ActionResult LotBuy(int LotID)
+        {
+            Lot lot = repository.GetLotById(LotID);
+            return View(lot);
+        }
+    }
+}
